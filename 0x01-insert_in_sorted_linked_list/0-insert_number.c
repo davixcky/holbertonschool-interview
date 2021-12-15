4378633b368235d3ea44e8e9961f44982aee72c5
David Orozco <orozco.davidj02@gmail.com>
2021-12-15 15:21:49 -0500

    fix: inserting node in the tail of the list

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -36,8 +36,7 @@ listint_t *insert_node(listint_t **head, int number)
 		;
 
 	new_node->next = tmp->next;
-	if (tmp->next)
-		tmp->next = new_node;
+	tmp->next = new_node;
 
 	return (new_node);
 }
