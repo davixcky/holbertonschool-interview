d750ca2b684404a6c5d3832c16c5daaf716f256b
David Orozco <orozco.davidj02@gmail.com>
2022-01-26 21:48:56 -0500

    add file projects

---
+++ 0x06-log_parsing/0-stats.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import sys
+
+from fastapi import status
+
+
+total_size = 0
+i = 0
+info_codes = {
+    '200': 0, 
+    '301': 0, 
+    '400': 0, 
+    '401': 0, 
+    '403': 0, 
+    '404': 0, 
+    '405': 0,
+    '500': 0,
+}
+
+def print_data():
+    print('File size: {}'.format(int(total_size)))
+    for status_code, value in info_codes.items():
+        if value == 0:
+            continue
+
+        print('{}: {}'.format(status_code, value))
+
+try:
+    while True:
+        current_line = sys.stdin.readline().strip()
+        tokens = current_line.split(' ')
+        current_size, status_code = tokens[-1], tokens[-2]
+        total_size += float(current_size)
+        info_codes[status_code] += 1
+        i += 1
+
+        if i == 10:
+            print_data()
+            i = 0
+
+except KeyboardInterrupt:
+    print_data()
+    sys.stdout.flush()
