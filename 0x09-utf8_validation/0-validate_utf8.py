bf13d7674adfe28882a7fdff9b276bda73b0bc29
David Orozco <orozco.davidj02@gmail.com>
2022-02-18 09:32:41 -0500

    change validation logic

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -4,5 +4,23 @@
 
 def validUTF8(data):
     """Check if a list of integers is a valid utf-8 set"""
-    result = map(lambda x: x >= 0 and x <= 255, data)
-    return all(result)
+    nbytes = 0
+
+    byte1 = 1 << 7
+    byte2 = 1 << 6
+
+    for number in data:
+        b = 1 << 7
+        if nbytes == 0:
+            while b & number:
+                nbytes += 1
+                b = b >> 1
+            if nbytes == 0:
+                continue
+            if nbytes == 1 or nbytes > 4:
+                return False
+        else:
+            if not (number & byte1 and not (number & byte2)):
+                return False
+        nbytes -= 1
+    return nbytes == 0
