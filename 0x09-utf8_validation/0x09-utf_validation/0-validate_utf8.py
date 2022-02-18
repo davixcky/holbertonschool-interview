3784d63ad8660b6579d18e7e0775d6e85eabfba3
David Orozco <orozco.davidj02@gmail.com>
2022-02-18 06:31:05 -0500

    rename file directory

---
+++ 0x09-utf8_validation/0x09-utf_validation/0-validate_utf8.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Module for utf-8 utility""""
+
+
+def validUTF8(data):
+    """Check if a list of integers is a valid utf-8 set"""
+    result = map(lambda x: x >= 0 and x <= 255, data)
+    return all(result)
